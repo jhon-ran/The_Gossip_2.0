ad8798a4cbbbf26d1d6808078b39baed44c82455
jhon-ran <jhonnatan.rangel@gmail.com>
2020-05-06 23:40:15 +0200

    refactoring comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,14 @@ class CommentsController < ApplicationController
     redirect_to gossip_path(@gossip)
   end
  
+
+  def destroy
+    @gossip = Gossip.find(params[:gossip_id])
+    @comment = @gossip.comments.find(params[:id])
+    @comment.destroy
+    redirect_to gossip_path(@gossip)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
