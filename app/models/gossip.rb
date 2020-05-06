e6c915136cd3869a71556e2d6c0e75ccd57dfea9
jhon-ran <jhonnatan.rangel@gmail.com>
2020-05-06 23:43:57 +0200

    added delete comments functionality

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,6 +1,7 @@
 class Gossip < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  # if gossip comment is deleted when gossip is
+  has_many :comments, dependent: :destroy
   
   # validations
   validates :title, presence: true, length: { minimum: 3, maximum: 14}
